b2cf2e1c0caef6f71a4e59dad5ce53bc60845345
Kevin Nilsson <kevin_nilsson98@hotmail.com>
2017-10-20 00:23:09 +0200

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,12 @@
+--[[
+    [Game] Untitled
+    @version 0.0, YYYY-MM-DD
+    @author Kevin Nilsson
+--]]
+
+
+function love.load() end
+function love.update() end
+function love.draw() end
+function love.keypressed( key ) end
+function love.mousepressed( x, y, button ) end
