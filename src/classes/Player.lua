ec16c6be8626b802ccc0a9d868b01cab51e8b599
Kevin Nilsson <kevin_nilsson98@hotmail.com>
2017-10-20 21:28:48 +0200

    Initiate some classes

---
+++ src/classes/Player.lua
@@ -0,0 +1,40 @@
+--[[
+    [Class] Player
+    @version 1.0, 2017-10-20
+    @author Kevin Nilsson
+--]]
+
+
+
+local Entity = require( "src/classes/Entity" )
+local Player = Entity:extend()
+
+
+
+--[[
+    Initiates a new player
+
+    @param int x, "The x coordinate"
+    @param int y, "The y coordinate"
+--]]
+function Player:new(x, y)
+    Entity.super.new( self, x, y )
+end
+
+
+
+--[[
+    Draws the player
+--]]
+function Player:draw() end
+
+
+
+--[[
+    Updates stuff for the player
+--]]
+function Player:update(dt) end
+
+
+
+return Player
